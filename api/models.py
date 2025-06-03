780c1136e91eda54fb4c8350d99c3467a741e155
CHUNG, Fu Chuan <2941673c@student.gla.ac.uk>
2025-06-03 15:07:22 +0800

    feat: add user in Order model

--- api/models.py
+++ api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Order(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=True, blank=True)
     order_number = models.IntegerField()
     total_price = models.IntegerField()
     created_time = models.DateTimeField(auto_now_add=True)
