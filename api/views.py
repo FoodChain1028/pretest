4ee2013fde6ab5b71b230eea32d04fa5d8c583b5
CHUNG, Fu Chuan <2941673c@student.gla.ac.uk>
2025-06-03 14:56:56 +0800

    fix: modified the wrong error code

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponseBadRequest, JsonResponse
 from rest_framework.decorators import api_view
+from rest_framework import status
 from .models import Order
 
 ACCEPTED_TOKEN = ('omni_pretest_token')
@@ -9,18 +10,23 @@ ACCEPTED_TOKEN = ('omni_pretest_token')
 def import_order(request):
     token = request.data.get('token')
 
+    if not token:
+        return JsonResponse({"message": "Token is required"}, status=status.HTTP_401_UNAUTHORIZED)
+    
     if token != ACCEPTED_TOKEN:
-        return HttpResponseBadRequest("Invalid Access Token")
+        return JsonResponse({"message": "Invalid Access Token"}, status=status.HTTP_401_UNAUTHORIZED)
     
     # check if order_number and total_price exists
     if not request.data.get('order_number'):
-        return HttpResponseBadRequest("Order number is required")
-    if not isinstance(request.data.get('order_number'), int):
-        return HttpResponseBadRequest("Order number must be an integer")
+        return JsonResponse({"message": "Order number is required"}, status=status.HTTP_400_BAD_REQUEST)
     if not request.data.get('total_price'):
-        return HttpResponseBadRequest("Total price is required")
+        return JsonResponse({"message": "Total price is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+    if not isinstance(request.data.get('order_number'), int):
+        return JsonResponse({"message": "Order number must be an integer"}, status=status.HTTP_400_BAD_REQUEST)
+
     if not isinstance(request.data.get('total_price'), int):
-        return HttpResponseBadRequest("Total price must be an integer")
+        return JsonResponse({"message": "Total price must be an integer"}, status=status.HTTP_400_BAD_REQUEST)
 
     order_number = request.data.get('order_number')
     total_price = request.data.get('total_price')
@@ -30,4 +36,4 @@ def import_order(request):
         total_price=total_price,
     )
 
-    return JsonResponse({"message": "Order imported successfully", "order_id": order.id})
+    return JsonResponse({"message": "Order imported successfully", "order_id": order.id, "order_number": order.order_number, "total_price": order.total_price})
